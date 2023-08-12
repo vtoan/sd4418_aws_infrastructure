3b3b2e625bbdf6435812cfdb6b03e85d8fd333e5
Dang Viet Toan <toan.dangviet@nashtechglobal.com>
2023-08-12 12:51:05 +0700

    init proj

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "az_client_secret" {
+  type        = string
+  sensitive   = true
+}
+
+variable "az_client_id" {
+  type        = string
+}
+
+variable "az_sub_id" {
+  type        = string
+}
+
+variable "az_tenant_id" {
+  type        = string
+}

eff631fbc45705f463b6b551dc2945e60d526550
Dang Viet Toan <toan.dangviet@nashtechglobal.com>
2023-08-13 11:11:44 +0700

    Add ACR, AKS

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,11 +30,31 @@ resource "azurerm_virtual_network" "example" {
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "example-subnet-internal"
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
 }
 
 ### SET UP BASTION
@@ -66,40 +86,19 @@ resource "azurerm_subnet" "internal" {
 #   }
 # }
 
-resource "azurerm_public_ip" "example" {
-  name                = "example-public-ip1"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  allocation_method   = "Static"
-}
-
-resource "azurerm_network_interface" "internal" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
-  }
-}
-
-
-### SETUP VIRTUAL MACHINE
+## SETUP VIRTUAL MACHINE
 resource "azurerm_linux_virtual_machine" "example" {
   name                = "example-machine"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = "Standard_F2"
 
-  admin_username      = "adminuser"
-  admin_password = "Pa55w0rd$"
+  admin_username                  = "adminuser"
+  admin_password                  = "Pa55w0rd$"
   disable_password_authentication = false
 
   network_interface_ids = [
-    azurerm_network_interface.internal.id,
+    azurerm_network_interface.example.id,
   ]
 
 
@@ -113,5 +112,32 @@ resource "azurerm_linux_virtual_machine" "example" {
     offer     = "0001-com-ubuntu-server-focal"
     sku       = "20_04-lts"
     version   = "latest"
+  }
+}
+
+### SETUP CONATINER REGISTRY
+resource "azurerm_container_registry" "example" {
+  name                = "exampleArc1308"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+### SETUP K8S
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
   }
 }

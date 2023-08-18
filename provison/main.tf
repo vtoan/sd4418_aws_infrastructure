2ed4ffb3d20625e197f4e6b4289ede07177b255f
Toan Dang <dtoan2805@gmail.com>
2023-08-19 03:42:02 +0700

    Update node SKU

--- provison/main.tf
+++ provison/main.tf
@@ -61,7 +61,7 @@ resource "azurerm_kubernetes_cluster" "example" {
   default_node_pool {
     name           = "default"
     node_count     = 1
-    vm_size        = "Standard_B2s"
+    vm_size        = "Standard_DS2_v2"
     vnet_subnet_id = data.azurerm_subnet.kubesubnet.id
   }
 
